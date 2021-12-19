a4dfe1fa83981794c6bc5f25d1c0ec61db2bd6bb
Otavio Brito <otaviofbrito@gmail.com>
2021-12-18 21:47:54 -0300

    update-de18

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,10 @@ const express = require('express')
 const routes = express.Router()
 const ProfileController = require('./controllers/ProfileControllers')
 const JobController = require('./controllers/JobControllers')
+const DashboardController = require('./controllers/DashboardController')
 
 //require response
-routes.get('/', JobController.index)
+routes.get('/', DashboardController.index)
 routes.get('/job', JobController.create)
 routes.post('/job', JobController.save)
 routes.get('/job/:id', JobController.show)
